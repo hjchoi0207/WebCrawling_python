e90f969ca1c1dd516a1ace85708bff235a943502
hjchoi0207 <44575072+hjchoi0207@users.noreply.github.com>
2020-09-01 21:53:55 +0900

    수정본 업로드

--- Crawlier_beautifulsoup_2.py
+++ Crawlier_beautifulsoup_2.py
@@ -17,8 +17,8 @@ soup = BeautifulSoup(html, 'html.parser')  # Beautifulsoup로 분석
 # find만 사용 시 결과 값은 하나
 # sh_blog_title을 가진 class를 모두 찾아라
 # 우리가 필요한 것은 href태그와 title 태그의 내용
-# title = soup.find_all(class_='sh_blog_title')
-# print(len(title))  # 결과는 10 : 네이버 블로그 페이지당 10개 씩 나오기 때문
+title = soup.find_all(class_='sh_blog_title')
+print(len(title))  # 결과는 10 : 네이버 블로그 페이지당 10개 씩 나오기 때문
 
 for i in title:
     print(i.attrs['title'])
